ddc55fd39a2a35c6a0d1f2c5173ad4fdc4059164
Stripthis <remy@anon>
2013-02-25 14:29:22 +0100

    use 1 tab for indentations not 2 or 4 spaces

--- js/progressbar.js
+++ js/progressbar.js
@@ -1,8 +1,7 @@
 //temp values:
-										var updateUrl = '1496';
-										var progressCount = 1496;
-										var progressTarget = 5000;
-
+var updateUrl = '1496';
+var progressCount = 1496;
+var progressTarget = 5000;
 
 /* time to wait to fetch next value from server (in milliseconds) */
 var fetchFrequency = 4000;
@@ -28,89 +27,92 @@ var targetValue = 0;
 var firstFetch = true;
 var currentWidth = 0;
 
-$.ajaxSetup({ cache: false });
+$.ajaxSetup({
+	cache: false
+});
 $(document).ready(function () {
-    controlWidth = parseInt($('.meter-wrap').css('width').replace('px', ''));
-    $('.meter-text span').html(0);
-    UpdateProgress();
+	controlWidth = parseInt($('.meter-wrap').css('width').replace('px', ''));
+	$('.meter-text span').html(0);
+	UpdateProgress();
 });
 
 function UpdateProgress() {
-    if (paused) {
-        setTimeout("UpdateProgress();", fetchFrequency);
-        return;
-    }
+	if (paused) {
+		setTimeout("UpdateProgress();", fetchFrequency);
+		return;
+	}
 
-    //$.get(updateUrl, function (data) {
-        paused = true;
+	//$.get(updateUrl, function (data) {
+	paused = true;
 
-        var value = updateUrl;
-        if (value < currentValue)
-            currentValue = 0;
-        targetValue = value;
-        initialStep = Math.ceil((value * updateSpeed) / initialAnimationTotalDuration, 0);
-        if (initialStep == 0) initialStep = 1;
+	var value = updateUrl;
+	if (value < currentValue) currentValue = 0;
+	targetValue = value;
+	initialStep = Math.ceil((value * updateSpeed) / initialAnimationTotalDuration, 0);
+	if (initialStep == 0) initialStep = 1;
 
-        animateProgress();
-    //});
+	animateProgress();
+	//});
 }
 
 function animateProgress() {
-    if (currentValue >= targetValue) {
-        paused = false;
-        setTimeout("UpdateProgress();", fetchFrequency);
-        return;
-    }
+	if (currentValue >= targetValue) {
+		paused = false;
+		setTimeout("UpdateProgress();", fetchFrequency);
+		return;
+	}
 
-    if (firstFetch) {
-        if (targetValue - currentValue > initialStep)
-            currentValue += initialStep;
-        else {
-            currentValue = targetValue;
-            firstFetch = false;
-        }
-    } else {
-        currentValue++;
-    }
+	if (firstFetch) {
+		if (targetValue - currentValue > initialStep) currentValue += initialStep;
+		else {
+			currentValue = targetValue;
+			firstFetch = false;
+		}
+	} else {
+		currentValue++;
+	}
 
-    var percent = currentValue / progressTarget;
-    if (percent > 1) {
-        percent = 1;
-    }
+	var percent = currentValue / progressTarget;
+	if (percent > 1) {
+		percent = 1;
+	}
 
-    $('.meter-text span').html(addCommas(currentValue));
-    var textWidth = $('.meter-text span').width();
-    var progressWidth = controlWidth * percent;
+	$('.meter-text span').html(addCommas(currentValue));
+	var textWidth = $('.meter-text span').width();
+	var progressWidth = controlWidth * percent;
 
-    if (progressWidth < textWidth) {
-        progressWidth = textWidth + 10;
-    }
+	if (progressWidth < textWidth) {
+		progressWidth = textWidth + 10;
+	}
 
-    if (progressWidth > currentWidth) {
-        currentWidth = progressWidth;
-    }
-    $('.meter-value').animate({ width: currentWidth }, { duration: animationDuration });
+	if (progressWidth > currentWidth) {
+		currentWidth = progressWidth;
+	}
+	$('.meter-value').animate({
+		width: currentWidth
+	}, {
+		duration: animationDuration
+	});
 
-    /* adjust text position to ensure visibility */
-    var difference = $('.meter-value').width() - textWidth;
-    if (difference < 0) {
-        $('.meter-text').css('margin-right', (difference - 5) + 'px');
-    }
-    else {
-        $('.meter-text').css('margin-right', '5px');
-    }
+	/* adjust text position to ensure visibility */
+	var difference = $('.meter-value').width() - textWidth;
+	if (difference < 0) {
+		$('.meter-text').css('margin-right', (difference - 5) + 'px');
+	} else {
+		$('.meter-text').css('margin-right', '5px');
+	}
 
-    setTimeout("animateProgress()", updateSpeed);
+	setTimeout("animateProgress()", updateSpeed);
 }
 
 function addCommas(nStr) {
-    nStr += '';
-    x = nStr.split('.');
-    x1 = x[0];
-    x2 = x.length > 1 ? '.' + x[1] : '';
-    var rgx = /(\d+)(\d{3})/;
-    while (rgx.test(x1)) {
-        x1 = x1.replace(rgx, '$1' + ',' + '$2');
-    }
-    return x1 + x2;
+	nStr += '';
+	x = nStr.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
 }
