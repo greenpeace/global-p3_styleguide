a7ef944627c1634d32a88115e402c093e510dc79
funkygibbing <fish@ptang>
2013-09-05 16:11:02 +1000

    Adding form validation based on http://jqueryvalidation.org/
    Some native HTML5 form validation features included also.

---
+++ js/v03/plugins.js
@@ -0,0 +1,88 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function() {
+    };
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'];
+    var l = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (l--) {
+        method = methods[l];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Validation plugin 0.1
+;
+(function($) {
+    var _p3 = $.p3 || {},
+    defaults = {
+        jsonURL: 'https://www.greenpeace.org/api/p3/pledge/config.json',
+        tests: {
+            // Matches all alphanumeric characters, including accents plus . , - ' / 
+            // Note for end users: when overriding or creating tests,
+            // character strings must be doulbe escaped: \\ instead of \
+            // http://stackoverflow.com/questions/16572123/javascript-regex-invalid-range-in-character-class
+            alphaPlus: /^[A-Za-z\u00C0-\u017F\d\.\-\'\,\/]*$/,
+            numeric: /^[\d]*$/,
+            alpha: /^[\w]*$/,
+        }
+    }
+
+    _p3.validation = function(el, config) {
+
+        // merge the default and the given data using 'deep' extend feature
+        // see. http://api.jquery.com/jQuery.extend/
+        config = $.extend(true, defaults, config || {});
+
+        Modernizr.load({
+            test: window.JSON,
+            nope: [
+                'js/v03/lib/json.min.js'
+            ],
+            complete: function() {
+                $.getJSON(config.jsonURL, function(data) {
+                    $.extend(true, config, data || {});
+
+//                    console.log(config);
+                    var tests = $.makeArray(config.tests);
+
+                    // Foreach data.tests ...
+                    $.each(config.tests, function (name, regexp) {
+                        console.log(name + ' ' +regexp);
+//                        $.map(test, function (regexp, name) {
+                            $.validator.addMethod(name, function(value, element) {
+                                console.log('Testing '+value+' against '+regexp);
+                                var reg = new RegExp(regexp);
+                                return this.optional(element) || reg.test(value);
+                            });     
+//                        });
+                        
+                    });
+                    
+                    // And go
+                    $(el).validate(config);
+
+                }).error(function() {
+                    // Placeholder for failure to obtain JSON
+                    console.log('JSON failed to load: ' + config.jsonURL);
+
+                    //$('input[type=submit]',el).attr('disabled','disabled').addClass('disabled');
+                });
+            }
+        });
+    }
+    
+    // Overwrite previous namespaced object
+    $.p3 = _p3;
+
+}(jQuery));
