156ec34d4bef8baf3fef528621d48bf05cd2e2dd
Florian Engel <florian@more-onion.com>
2013-09-16 18:16:06 +0200

    jquery temporarily added along with narrow.js which is used for media query fallback

---
+++ js/default/compat/narrow.js
@@ -0,0 +1,30 @@
+/*
+ * This file is a fallback to the narrow.js in our starterkits.
+ * The size classes (.four, .five, ...) are used by the layout.less file
+ *
+ * This file can be overriden by any derivates (starterkits).
+ */
+function checkNarrow(){
+  var size = {
+    four: 400,
+    five: 500,
+    six: 600,
+    sixfive: 650,
+    seven: 700,
+    sevensome: 768,
+    eightfive: 850,
+    nine: 900,
+    tablet: 500,
+    desktop: 950
+  };
+  jQuery.each(size, function(cls, size) {
+    if (jQuery(window).width() >= size) {
+      jQuery('body').addClass(cls);
+    } else {
+      jQuery('body').removeClass(cls);
+    }
+  });
+}
+
+jQuery(window).resize(checkNarrow);
+jQuery(window).ready(checkNarrow);
