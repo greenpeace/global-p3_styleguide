45ea22ffe9c7fe52935f3fc5c6627e90f92e0c5a
Ray Walker <hello@raywalker.it>
2013-09-19 13:00:46 +1000

    Pledge counter animation
    Adapted v02 code to suit p3 namespace, and allowing for event triggering to reuse existing JSON fetches

--- js/v03/main.js
+++ js/v03/main.js
@@ -1,10 +1,23 @@
 ;
 (function($) {
+    var pledgeJSON = 'js/v03/json_testing/pledges.json';
     
     $(document).ready(function() {
         $.p3.validation('#action-form-pledgeName', {
             jsonURL: 'js/v03/json_testing/rules.json'
         });
+        
+        var counter = $.p3.pledge_counter('#action-counter', {
+            jsonURL: pledgeJSON,
+//            eventDriven: true,
+            uuid: '550e8402-e29b-41d4-a716-446655444563'
+        });
+        
+//        $.getJSON(pledgeJSON, function (json) {
+//            $(counter.config.dataElement).data(counter.config.dataNamespace, json);
+//            counter.fetchComplete();
+//        });
+        
     });
 
 }(jQuery));
