4c31213e46a08b79e685ea18492514a1c5b8086f
funkygibbing <geek@raywalker.it>
2013-09-25 23:55:09 +1000

    p3.remember_me_cookie.js
    - with bonus extra: RememberRememberMe

--- js/v03/main.js
+++ js/v03/main.js
@@ -32,13 +32,15 @@
 //            jsonURL: 'js/v03/json_testing/rules_revised.json',
 //        });
         
+        $.p3.remember_me_cookie('#action-form');
+        
         $.p3.pledge_counter('#action-counter', {
             jsonURL: pledgeJSON,
             uuid: '550e8402-e29b-41d4-a716-446655444563'
         });
   
         $.p3.pledge_with_email_only('#action-form', {
-            jsonURL: 'js/v03/json_testing/signer_error_fields.json',
+            signerCheckURL: 'js/v03/json_testing/signer_error_fields.json?user=fish',
             pageUUID: '550e8401-e29b-41d4-a716-446678440294',
             validationRulesURL: 'js/v03/json_testing/rules_revised.json'
         });
