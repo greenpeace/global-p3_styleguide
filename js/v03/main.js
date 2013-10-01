07288630d0d3afb666cbcda87cec6ef01828ba68
funkygibbing <geek@raywalker.it>
2013-10-01 16:22:50 +1000

    p3.social_sharing.js
    Link-based social network sharing.
    Obtains counts from json endpoint

--- js/v03/main.js
+++ js/v03/main.js
@@ -22,16 +22,34 @@
 }());
 
 
-(function($) {
+(function($, M) {
     'use strict';
     var pledgeJSON = 'js/v03/json_testing/pledges.json';
     
     $(document).ready(function() {
-//  Form validation is included in p3.pledge_with_email_only
+// Form validation is included in p3.pledge_with_email_only
+//  
 //        $.p3.validation('#action-form-pledgeName', {
 //            jsonURL: 'js/v03/json_testing/rules_revised.json',
 //        });
         
+// Show placeholders using javascript ?        
+// 
+//        M.load({
+//            test: M.input.placeholder,
+//            nope: [
+//                'js/default/compat/placeholders.min.js'
+//            ],
+//            complete: function() {
+//                // Doesn't like 
+//                $('input[placeholder]').each(function () {
+//                    $(this).focus();                    
+//                });
+//                
+//                $('body').focus();
+//            }
+//        });
+               
         $.p3.remember_me_cookie('#action-form');
         
         $.p3.pledge_counter('#action-counter', {
@@ -45,6 +63,19 @@
             validationRulesURL: 'js/v03/json_testing/rules_revised.json'
         });
         
+        $.p3.social_sharing('#action-social-share', {
+            jsonURL: 'js/v03/json_testing/social_simple.json',
+            networks: {
+                twitter: {
+                    title: window.document.title
+                },
+                pinterest: {
+                    image: 'http://www.greenpeace.org/international/Global/international/artwork/other/2010/openspace/bigspace-photo.jpg',
+                    description: window.document.title
+                }
+            }
+        });
+        
     });
 
-}(jQuery));
+}(jQuery, Modernizr));
