4f30fede03871c258f50812e498bf4d86bc66c65
funkygibbing <geek@raywalker.it>
2013-11-02 16:58:45 +1100

    add validateDelegate for new version of validation plugin

--- js/action-template-main.js
+++ js/action-template-main.js
@@ -15,15 +15,15 @@
         page: 269648,                               // ID of the current page
         key:  '78d245e17c455859b4863ad34674f2b8'    // API access key - tied to the referring domain
     },
-    // Demonstration showing parameters passed in the URL (including spurious parameter)
-    pledgeURL = 'http://greenpeace.relephant.nl/international/en/api/v2/pledges/',
-    pledgeTesting = 'json/pledges.json',
+    // API URL Demonstration showing parameters passed in the URL (including spurious parameter)
+    pledgeURL = 'http://greenpeace.relephant.nl/international/en/api/v2/pledges/?fish=salmon',
+    pledgeTesting = 'json/pledges.json?fish=salmon',
+    // API URL to test if user can submit form using only email
     signerCheckURL = 'http://greenpeace.relephant.nl/international/en/api/v2/pledges/signercheck/',
     signerCheckTesting = 'json/signer_error_fields.json',
-    // Demonstrating a blank URL, which will have required parameters added in the function call
+    // API URL for form validation rules
     validationURL = 'http://greenpeace.relephant.nl/international/en/api/v2/pledges/validation/',
     validationTesting = 'json/rules_revised.json';
-
     $.ajaxSetup({cache: 1});
 
     $(document).ready(function() {
@@ -79,7 +79,7 @@
 
         // Update social share counts
         $.p3.social_sharing('#action-social-share', {
-            jsonURL: 'js/v03/json_testing/social_simple.json',
+            jsonURL: 'json/social_simple.json',
             networks: {
                 twitter: {
                     title: window.document.title
