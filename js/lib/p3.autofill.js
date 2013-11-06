24a39ed6f505f4c25700c27270219d450444da47
funkygibbing <geek@raywalker.it>
2013-11-06 16:33:32 +1100

    Bugfix for emails in values

--- js/lib/p3.autofill.js
+++ js/lib/p3.autofill.js
@@ -4,7 +4,7 @@
  *
  * @fileOverview    Automatically fill form fields from GET parameters
  * @author          <a href="mailto:hello@raywalker.it">Ray Walker</a>
- * @version         0.0.1
+ * @version         0.0.2
  * @copyright       Copyright 2013, Greenpeace International
  * @license         MIT License (opensource.org/licenses/MIT)
  * @requires        <a href="http://jquery.com/">jQuery 1.7+</a>,
@@ -24,9 +24,10 @@
 
         // Populate form fields from GET variables
         $.each($.p3.request(w.location.href).parameters, function(field, value) {
-            $(':radio[name=' + field + ']', $el).filter('[value=' + value + ']').prop('checked', true);
-            $(':input[name=' + field + ']', $el).val(value);
-            $(':input[value=' + field + ']', $el).val(value);
+            value = value.replace('#','');
+            $(':radio[name="' + field + '"]', $el).filter('[value="' + value + '"]').prop('checked', true);
+            $(':input[name="' + field + '"]', $el).val(value);
+            $(':input[value="' + field + '"]', $el).val(value);
         });
     };
 
