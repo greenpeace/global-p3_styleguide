8c887d8f66d6e9997d861bab65d7987172ebbecb
funkygibbing <geek@raywalker.it>
2013-11-14 21:53:34 +1100

    p3.social_sharing: Added popup placement and size as a configurable parameter

--- js/lib/p3.social_sharing.js
+++ js/lib/p3.social_sharing.js
@@ -1,79 +1,78 @@
 /**!
  * Social Private Sharing for Greenpeace Action Template v0.3
- * INCOMPLETE
+ *
  * @fileOverview    Enables social sharing without compromising privacy,
  *                  Obtains share counts from JSON endpoint
  * @copyright       Copyright 2013, Greenpeace International
  * @license         MIT License (opensource.org/licenses/MIT)
- * @version         0.1.1
+ * @version         0.1.2
  * @author          Ray Walker <hello@raywalker.it>
  * @requires        <a href="http://jquery.com/">jQuery 1.6+</a>,
  *                  <a href="http://modernizr.com/">Modernizr</a>,
  * @example         $.p3.social_sharing('#action-social-share'[, options]);
- *
- * @todo            Create a modal iframe dialogue for sharing
- *
  */
 /*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, unused:true, curly:true, browser:true, devel:true, jquery:true, indent:4, maxerr:50 */
-/*global Modernizr */
+/*global jQuery, Modernizr */
 
 (function($, M, w) {
     'use strict';
 
     var _p3 = $.p3 || {},
-            defaults = {
-                pageURL: false,
-                popup: {
-                    width: 800,
-                    height: 600
+        defaults = {
+            pageURL: false,
+            popup: {
+                top: 200,
+                left: 100,
+                width: 550,
+                height: 350
+            },
+            networks: {
+                facebook: {
+                    enabled: true,
+                    url: 'http://www.facebook.com/sharer.php?u=__REFERRER__',
+                    count: 0
                 },
-                networks: {
-                    facebook: {
-                        enabled: true,
-                        url: 'http://www.facebook.com/sharer.php?u=__REFERRER__',
-                        count: 0
-                    },
-                    twitter: {
-                        enabled: true,
-                        url: 'https://platform.twitter.com/widgets/tweet_button.html?original_referer=__REFERRER__&source=tweetbutton&text=__TITLE__&url=__REFERRER__&via=__ACCOUNT__',
-                        count: 0,
-                        title: false,
-                        account: 'greenpeace'
-                    },
-                    google: {
-                        enabled: true,
-                        url: 'https://plus.google.com/share?url=__REFERRER__',
-                        count: 0
-                    },
-                    linkedin: {
-                        enabled: true,
-                        url: 'https://www.linkedin.com/cws/share?url=__REFERRER__&original_referer=__REFERRER__',
-                        count: 0
-                    },
-                    pinterest: {
-                        enabled: true,
-                        url: 'http://pinterest.com/pin/create/button/?url=__REFERRER__&media=__IMAGE__&description=__DESCRIPTION__',
-                        count: 0,
-                        image: false,
-                        description: false
-                    }
+                twitter: {
+                    enabled: true,
+                    url: 'https://twitter.com/intent/tweet?original_referer=__REFERRER__&source=tweetbutton&text=__TITLE__&url=__REFERRER__&via=__ACCOUNT__',
+                    count: 0,
+                    title: false,
+                    account: 'greenpeace'
                 },
-                precision: 1,
-                jsonURL: 'https://www.greenpeace.org/api/p3/pledge/social.json'
-            };
+                google: {
+                    enabled: true,
+                    url: 'https://plus.google.com/share?url=__REFERRER__',
+                    count: 0
+                },
+                linkedin: {
+                    enabled: true,
+                    url: 'https://www.linkedin.com/cws/share?url=__REFERRER__&original_referer=__REFERRER__',
+                    count: 0
+                },
+                pinterest: {
+                    enabled: true,
+                    url: 'http://pinterest.com/pin/create/button/?url=__REFERRER__&media=__IMAGE__&description=__DESCRIPTION__',
+                    count: 0,
+                    image: false,
+                    description: false
+                }
+            },
+            precision: 1,
+            jsonURL: 'https://www.greenpeace.org/api/p3/pledge/social.json'
+        };
 
     _p3.social_sharing = function(el, options) {
 
         var config = $.extend(true, defaults, options || {}),
-                $el = $(el),
-                support = M.csstransitions;
+            $el = $(el),
+            support = M.csstransitions;
 
         function addCommas(int) {
             var nStr = int + '',
-                    x = nStr.split('.'),
-                    x1 = x[0],
-                    x2 = (x.length > 1 && (x[1] * 1.0) > 0 && int < 100) ? '.' + x[1].replace(/0+$/, "") : '',
-                    rgx = /(\d+)(\d{3})/;
+                x = nStr.split('.'),
+                x1 = x[0],
+                x2 = (x.length > 1 && (x[1] * 1.0) > 0 && int < 100) ? '.' + x[1].replace(/0+$/, "") : '',
+                rgx = /(\d+)(\d{3})/;
             while (rgx.test(x1)) {
                 x1 = x1.replace(rgx, '$1' + ',' + '$2');
             }
@@ -90,8 +89,8 @@
             }
 
             var ordinals = ["", "K", "M", "G", "T", "P", "E"],
-                    n = int,
-                    ord = 0;
+                n = int,
+                ord = 0;
 
             while (n > 1000) {
                 n /= 1000;
@@ -102,11 +101,12 @@
         }
 
         function init() {
+            
             if (support) {
                 var $modal = $('<div id="social-modal"><div class="modal-content"><h3>Share this page<button class="close">Close</button></h3><iframe></iframe></div></div>'),
                     $overlay = $('<div id="social-overlay"></div>');
                 $('body').append($modal).append($overlay);
-                $overlay.add('#social-modal button').click( function () {
+                $overlay.add('#social-modal button').click(function() {
                     $modal.removeClass('modal-show');
                 });
             }
@@ -119,9 +119,9 @@
             $.each(config.networks, function(network, data) {
                 if (data.enabled !== false) {
                     var $li = $('li.' + network, $el),
-                            $counter = $('.counter span', $li),
-                            $a = $('a', $li),
-                            url = data.url;
+                        $counter = $('.counter span', $li),
+                        $a = $('a', $li),
+                        url = data.url;
 
                     switch (network) {
                         case 'pinterest':
@@ -156,11 +156,11 @@
                     $a.attr({
                         href: url,
                         target: '_blank'
-                    }).click(function (e) {
+                    }).click(function(e) {
 
                         e.preventDefault();
 
-                        if (support && (network === 'facebook' || network === 'twitter') ) {
+                        if (support && network === 'facebook') {
                             // only facebook currently permits sharing in an
                             // iframe due to cross domain policies
                             var $iframe = $('iframe', $modal);
@@ -173,7 +173,7 @@
 
                         } else {
                             // sigh
-                            w.open(url, 'Share this page', 'left=200,top=100,width=550,height=350,toolbar=1,resizable=1');
+                            w.open(url, 'Share this page', 'left=' + config.popup.left + ',top=' + config.popup.top + ',width=' + config.popup.width + ',height=' + config.popup.height + ',toolbar=0,resizable=1');
                         }
                     });
 
