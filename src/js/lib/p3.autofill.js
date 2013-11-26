b2db67e4be3a4e3ef56842e32a8a5995b1e2f38b
funkygibbing <geek@raywalker.it>
2013-11-26 20:56:07 +1100

    p3.autofill.js - 0.0.6 - case insensitive names and values

--- src/js/lib/p3.autofill.js
+++ src/js/lib/p3.autofill.js
@@ -2,11 +2,12 @@
  * @name            p3.autofill.js
  * @fileOverview    Automatically fill form fields from GET parameters
  * @author          <a href="mailto:hello@raywalker.it">Ray Walker</a>
- * @version         0.0.5
+ * @version         0.0.6
  * @copyright       Copyright 2013, Greenpeace International
  * @license         MIT License (opensource.org/licenses/MIT)
  * @requires        <a href="http://jquery.com/">jQuery 1.7+</a>,
- *                  $.p3.request
+ *                  $.p3.request,
+ *                  $.p3.selectors
  * @example         $.p3.autofill('#action-form');
  *
  */
@@ -20,34 +21,6 @@
             delimiter: '|'
         };
 
-
-    $.expr[':'].nameNoCase = function(a, i, m) {
-        var name = $(a).attr('name'),
-            search = m[3];
-        if (!search || !name) {
-            return false;
-        }
-        return name.toUpperCase().indexOf(search.toUpperCase()) >= 0;
-    };
-
-    $.expr[':'].selectNoCase = function(a, i, m) {
-        var v = $('option', a).val() || '',
-            search = m[3];
-        if (!search || !v.length) {
-            return false;
-        }
-        return v.toUpperCase().indexOf(search.toUpperCase()) >= 0;
-    };
-
-    $.expr[':'].valueNoCase = function(a, i, m) {
-        var v = $(a).val() || '',
-            search = m[3];
-        if (!search || !v.length) {
-            return false;
-        }
-        return v.toUpperCase().indexOf(search.toUpperCase()) >= 0;
-    };
-
     _p3.autofill = function(el, options) {
 
         var $el = $(el),
@@ -57,9 +30,9 @@
         // Populate form fields from GET variables
         $.each($.p3.request(url).parameters, function(field, value) {
             if (value.indexOf(config.delimiter) > 0) {
-                var $checkboxes = $(':checkbox[name="' + field + '"]', $el);
+                var $checkboxes = $(':checkbox:nameNoCase("' + field + '")', $el);
                 $.each(value.split(config.delimiter), function(i, val) {
-                    $checkboxes.filter('[value="' + val + '"]').prop('checked', true);
+                    $checkboxes.filter(':valueNoCase("' + val + '")').prop('checked', true);
                 });
             } else {
                 //
