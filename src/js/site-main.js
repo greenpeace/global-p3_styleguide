9e53e15eef3f3e0f7b7b50ddee15408a0e46c8ae
Ray Walker <geek@raywalker.it>
2014-03-01 08:46:47 +1100

    Some documentation

--- src/js/site-main.js
+++ src/js/site-main.js
@@ -19,12 +19,31 @@
     // pretty checkboxes and radios via Formstone picker
     //$('form input[type=radio], form input[type=checkbox]:not(.mobilemenu-label)').picker();
 
-    // Initalise narrow.js if media queries are not supported
+    /**
+     * p3.narrow.js adds classes to the body indicating widths,
+     * for use in breakpoints for older browsers
+     */
     if (!M.mq('only all')) {
-         $.p3.narrow();
+        // Initalise narrow.js if media queries are not supported
+        $.p3.narrow();
     }
 
-    // Detect placeholder functionality
-    $('html').addClass((M.input.placeholder ? '' : 'no-') + 'placeholder');
+    /**
+     *  Placeholder options
+     */
+
+    // Force labels hidden
+//    $('html').addClass('placeholder');
+
+    // Initalise jquery.placeholder.js, a polyfill for old browser placeholders
+    // You should probably also force labels hidden
+    $('input, textarea, select').placeholder();
+
+    // Force labels shown
+//    $('html').addClass('no-placeholder');
+
+    // Show or hide labels depending on browser placeholder support
+    // $('html').addClass((M.input.placeholder ? '' : 'no-') + 'placeholder');
+
 
 }(jQuery, Modernizr, this));
