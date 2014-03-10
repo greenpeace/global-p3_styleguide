016154a0dbee3aa3788562e754802481bac67860
jlalovi <jlalovi@gmail.com>
2014-03-10 11:27:55 +0100

    merge from origin to kss

--- dist/js/site.js
+++ dist/js/site.js
@@ -1,30 +1,7 @@
 /**
  * @name		p3-styleguide
  * @version		v0.3.0
-<<<<<<< HEAD
- * @date		2014-03-01
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
- * @date		2014-02-24
-=======
- * @date		2014-02-27
->>>>>>> kss
-=======
- * @date		2014-02-28
->>>>>>> kss
-<<<<<<< HEAD
->>>>>>> develop
-=======
-=======
- * @date		2014-03-03
->>>>>>> kss
-=======
- * @date		2014-03-04
->>>>>>> kss
->>>>>>> develop
+ * @date		2014-03-10
  * @copyright	Copyright 2013, Greenpeace International
  * @source		https://github.com/greenpeace/p3_styleguide
  * @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later */
