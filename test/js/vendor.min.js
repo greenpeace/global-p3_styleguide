e98d076e7b688b9616aef16360c9b694fc5b035c
Ray Walker <geek@raywalker.it>
2014-03-22 12:26:03 +1100

    Task #PLANET-72 - Modernizr is currently the large default build, should optimise to only required tests grunt-modernizr (https://github.com/Modernizr/grunt-modernizr) now checks the contents of all src/ JS, and CSS files to determine which tests are required, then downloads the latest custom build automatically.
    Also modified Grunt tasks to copy and uglify the custom build.  Does not currently attempt to rebuild Modernizr each time src is changed, must manually re-run grunt modernizr

--- test/js/vendor.min.js
+++ test/js/vendor.min.js
@@ -1,7 +1,7 @@
 /**
  * @name		p3-styleguide
  * @version		v0.3.0
- * @date		2014-03-21
+ * @date		2014-03-22
  * @copyright	Copyright 2013, Greenpeace International
  * @source		https://github.com/greenpeace/p3_styleguide
  * @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later */
